ecd8de6255f1cd9c869c217bfe8d59483e864fa8
David Trossing <David.Trossing@elev.ga.ntig.se>
2023-08-30 10:56:15 +0200

    Cange

---
+++ Lucktext_Gnome/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is best game?");
+if(Console.ReadLine() == "Fortnite")
+{
+    Console.WriteLine("YES!!");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("NO!!");
+}
+Console.WriteLine("WTF!!");
